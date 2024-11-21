855924fabb912bb80bd3c8db7913e7151dd7a902
Hema <hemashah2315@gmail.com>
2024-11-21 19:15:24 +0530

    .

--- assgn/q1/Frame1.java
+++ assgn/q1/Frame1.java
@@ -3,10 +3,12 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -16,12 +18,17 @@ public class Frame1{
         JFrame frame = new JFrame("Simple Swing Application");
         frame.setSize(280,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(3,2));
+        frame.setLayout(new GridLayout(4,2));
 
         JLabel user = new JLabel("Username :");
         JTextField userarea = new JTextField(15);
         JLabel pass = new JLabel("Password :");
         JPasswordField passarea = new JPasswordField(15);
+        JRadioButton java = new JRadioButton("Java");
+        JRadioButton C = new JRadioButton("C");
+        ButtonGroup grp = new ButtonGroup();
+        grp.add(java);
+        grp.add(C);
         JButton reset = new JButton("Reset");
         JButton login = new JButton("Login");
         
@@ -29,6 +36,8 @@ public class Frame1{
         frame.add(userarea);
         frame.add(pass);
         frame.add(passarea);
+        frame.add(java);
+        frame.add(C);
         frame.add(reset);
         frame.add(login);
         
@@ -36,6 +45,7 @@ public class Frame1{
             public void actionPerformed(ActionEvent e){ 
             userarea.setText(null);
             passarea.setText(null);
+            grp.clearSelection();
             }
         });
         login.addActionListener(new ActionListener() {
